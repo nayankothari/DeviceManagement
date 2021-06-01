51ec5ee029cf4f006fb3e1e176f2c04fb1648d77
nayan <nnkothari86@gmail.com>
2021-06-01 17:33:29 +0530

    changes done

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from .views import DeviceData, DeviceDataUpdate,device_data_from_device, show_last_readings_of_device
+from .views import DeviceData, DeviceDataUpdate,device_data_from_device, show_last_readings_of_device, contact_to_device
 from django.urls import path
 
 urlpatterns = [
@@ -6,5 +6,6 @@ urlpatterns = [
     path('update/<str:device_id>/<str:relay>/<int:status>', DeviceDataUpdate),
     path('api/post_device_data/<str:site_prefix>', device_data_from_device),
     path('api/get_live_data/<str:site_prefix>', show_last_readings_of_device),
+    path('api/connect_to_device/', contact_to_device),
     # path('getid/<str:device_id>/<str:relay>/', GetDeviceId.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,9 @@ from .serializers import DeviceManagementSerializers
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from django.shortcuts import HttpResponse
 import json, os
+import paho.mqtt.publish as publish
 
 
 try: os.mkdir("temp_database")
@@ -52,7 +54,20 @@ def show_last_readings_of_device(request, **kwargs):
         site_prefix = kwargs.get("site_prefix")
         with open("temp_database//"+site_prefix+".json", "r") as f:
             data = f.read()
-        return Response(data, status=status.HTTP_200_OK)
+        return HttpResponse(data, status=status.HTTP_200_OK, content_type='application/json')
     except:
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+@api_view(["POST"])
+def contact_to_device(request,**kwargs):
+    try:
+        data = request.data
+        data1 = data.get("device_details")
+        prefix = data1.get("prefix")
+        message = json.dumps(data).encode("utf-8")
+        if prefix != "N/A":
+            publish.single(prefix, message, hostname="test.mosquitto.org", retain=True)
+        return Response(data)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
