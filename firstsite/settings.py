5bb8883da8b31dbd634b44cff85f4bb50af989c9
nayan <nnkothari86@gmail.com>
2021-06-02 10:17:23 +0530

    new changes

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eifm(osac4!-)a_p#bz5ngq32l3fmj^#lpp0o64ubjf@wc%g%q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
